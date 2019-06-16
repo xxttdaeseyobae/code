a22eb5e9a937d076093fc5dc90f24a02a1c91dd0
Manisha KC <caseymansa@gmail.com>
2019-06-16 12:08:01 +0545

    nav added

---
+++ topnavigation.php
@@ -0,0 +1,141 @@
+
+<html lang="en">
+<head>
+<meta charset="utf-8">
+  
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="dist/css/sb-admin-2.css" rel="stylesheet">
+      <link href="viewproducts.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+ <link rel="stylesheet" href="assets/css/viewproducts.css">
+ <link href="css/scrolling-nav.css" rel="stylesheet">
+    <link href="dist/css/login.css" rel="stylesheet">
+    <link href="dist/css/cookie.css" rel="stylesheet">
+    <link href="dist/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="dist/css/slider.css">
+
+<link rel="stylesheet" type="text/css" href="index.css"/> 
+<link rel="stylesheet" type="text/css" href="cooie.css"/>
+<link rel="stylesheet" type="text/css" href="bootstrap.min.css"/>
+<link rel="stylesheet" type="text/css" href="font-awesome.css"/>
+<link rel="stylesheet" type="text/css" href="main.css"/>
+<link rel="stylesheet" type="text/css" href="owl.carousel.css"/>
+    <link rel="stylesheet" href="assets/css/animate-3.7.0.css">
+    <link rel="stylesheet" href="assets/css/font-awesome-4.7.0.min.css">
+   
+    <link rel="stylesheet" href="assets/css/owl-carousel.min.css">
+    <link rel="stylesheet" href="assets/css/jquery.datetimepicker.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<title>Ilam Tea Garden</title>
+<!-- Fav Icon -->
+<link rel="shortcut icon" href="favicon.ico">
+
+<!-- Owl carousel -->
+<link href="dist/css/owl.carousel.css" rel="stylesheet">
+
+<!-- Bootstrap -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Font Awesome -->
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+<!-- Custom Style -->
+<link href="dist/css/main.css" rel="stylesheet">
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+  <script src="js/html5shiv.min.js"></script>
+  <script src="js/respond.min.js"></script>
+<![endif]-->
+
+</head><body>
+<!-- Top Bar start -->
+<div class="topbar-wrap">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 col-sm-6">
+        <div class="tpinfo"> <a href="#"><i aria-hidden="true">Ilam Tea Garden</i> </a> <a href="#"><i class="fa fa-map-marker" aria-hidden="true"></i> Ilam, Nepal</a> </div>
+      
+	  </div>
+      <div class="col-md-6 col-sm-6">
+        
+
+               <div class="toplinks">
+                <div id="search">
+      <form action="search.php" method="get">
+        <fieldset>
+          <input type="text" name="keyword" value="Search&hellip;"  onfocus="this.value=(this.value=='Search;')? '' : this.value ;" />
+          <input type="submit" name="search" id="go" value="Search" />
+        </fieldset>
+      </form>
+    </div>
+    <br class="clear" />
+              
+
+              <a href="login.php" >Login</a>
+              <a href="signup.php" >Sign up</a>
+              </div>
+   
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-2 col-sm-3 col-xs-12"> <a href="Index.php" class="logo"><img src="images/logo.jpg" height="50px", width="100px" alt="" /></a>
+    
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+        </div>
+        <div class="clearfix"></div>
+      </div>
+      <div class="col-md-10 col-sm-12 col-xs-12"> 
+        <!-- Nav start -->
+        <div class="navbar navbar-default" role="navigation">
+          <div class="navbar-collapse collapse" id="nav-main">
+            <ul class="nav navbar-nav">
+                   <li><a href="Index.php">Home </a>  </li>
+			  <li><a href="viewproducts.php">View Products</a></li>
+			
+				
+				<li><a href="booking.php">Product Booking</a></li>
+              <li><a href="forum.php">Forums</a></li>
+			  
+             
+            </ul>
+            <!-- Nav collapes end --> 
+          </div>
+          <div class="clearfix"></div>
+        </div>
+        <!-- Nav end --> 
+      </div>
+    </div>
+    <!-- row end --> 
+  </div>
+
+
